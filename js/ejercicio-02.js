0626bbb90b4d82cf49caed2f03342fb2f60c9625
Israel Avila <israel.avilaf@gmail.com>
2025-01-20 22:32:49 -0300

    Ejercicio 2

---
+++ js/ejercicio-02.js
@@ -0,0 +1,3 @@
+const array =  [`casa`, `programación`, `sol`, `javascript`, `texto`, `html`, `bootstrap`, `css`, `nodejs`];
+const mayorCinco = array.filter(palabra => palabra.length > 5);
+console.log(mayorCinco);
